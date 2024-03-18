199ccd41cb7b189bcbb634200f7e569d3b03c2db
IsoldaLulu <139595746+IsoldaLulu@users.noreply.github.com>
2024-03-17 22:51:20 -0700

    url_actualizada

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const BASE_API = 'https://plataforma-etica-api.nayarit.gob.mx';
+export const BASE_API = 'https://lbcomiteeticasaf2-101427307.us-east-1.elb.amazonaws.com';
 export const TOKEN = 'token';
+//export const BASE_API = 'https://plataforma-etica-api.nayarit.gob.mx';
